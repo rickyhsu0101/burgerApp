ab1d04498e0656753d8dd898e558a61fb96f014b
Ricky Hsu <rickyhsu0101@gmail.com>
2018-07-04 22:19:44 -0700

    heroku details

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
 DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
+
 USE burger_db;
+USE df70hz67223y74rf;
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
+
