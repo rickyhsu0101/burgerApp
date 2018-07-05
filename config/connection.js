6f2b1b966a3c46d0866ff4b8aa955770fcb51891
Ricky Hsu <rickyhsu0101@gmail.com>
2018-07-04 22:16:39 -0700

    jawsdb update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "FeAtHeRs147823690",
+        database: "burger_db"
+    });
+}
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "FeAtHeRs147823690",
-    database: "burger_db"
-});
 console.log("hellow");
 connection.connect(function(err) {
     if (err) {
